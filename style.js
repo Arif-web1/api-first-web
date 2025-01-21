1c501915f7985182c3eefca232a7eea5b5ef1384
Arif-web1~ <arifhossen1110@gmail.com>
2025-01-21 10:52:24 +0600

    first commit

--- style.js
+++ style.js
@@ -1,4 +1,4 @@
-const load_phone = async (searchText) => {
+const load_phone = async (searchText=14) => {
   const response = await fetch(
   ` https://openapi.programming-hero.com/api/phones?search=${searchText}`
   );
@@ -24,7 +24,7 @@ const container_all_data = (phone) => {
    phone=phone.slice(0, 12);
 
   phone.forEach((phone_element) => {
-    console.log(phone_element);
+    // console.log(phone_element);
     const div = document.createElement("div");
     div.classList = `card bg-base-100 w-96 shadow-xl bg-white pt-4`;
     div.innerHTML = `
@@ -37,7 +37,7 @@ const container_all_data = (phone) => {
               <h2 class="card-title font-semibold text-black">${phone_element.phone_name}</h2>
               <p>If a dog chews shoes whose shoes does he choose?</p>
               <div class="card-actions justify-end">
-                <button class="btn btn-primary">Buy Now</button>
+                <button onclick="id_details('${phone_element.slug}')" class="btn btn-primary">show details</button>
               </div>
             </div>
     `;
@@ -64,4 +64,33 @@ const stop_loading=()=>{
   const load_button=document.getElementById('loading');
   load_button.classList.add('hidden')
 
+}
+
+const id_details=async(id)=>{
+  const id_details=await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+const data=await id_details.json();
+const phone_document=data.data;
+// console.log(phone_document);
+search_details(phone_document)
+
+
+}
+
+const search_details=(phone_document)=>{
+const phone_name=document.getElementById('phn_name');
+phone_name.innerText=phone_document.name;
+const image_element=document.getElementById('img');
+image_element.src=phone_document.image;
+const storge=document.getElementById('storge_size');
+storge.innerText=phone_document.mainFeatures.storage;
+const display=document.getElementById('display_size');
+display.innerText=phone_document.mainFeatures.displaySize;
+const realse_date=document.getElementById('realse');
+realse_date.innerText=phone_document.mainFeatures.chipSet;
+
+
+console.log(phone_document);
+  
+  my_modal_5.showModal()
+  
 }
